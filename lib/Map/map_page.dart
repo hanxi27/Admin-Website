601f9ebbdd9cdc0f47f919fbabfc59ea4b025093
YeeWan1 <yeewan.fong123@gmail.com>
2024-07-24 16:11:58 +0800

    map page

--- lib/Map/map_page.dart
+++ lib/Map/map_page.dart
@@ -1,10 +1,128 @@
+// lib/Map/map_page.dart
 import 'package:flutter/material.dart';
+import 'region.dart' as myRegion; // Import the region.dart file with a prefix
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  String selectedCategory = '';
+
+  void _onRegionTap(String category) {
+    setState(() {
+      selectedCategory = category;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Map Page'),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Center(
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      // Define the aspect ratio of the floorplan image
+                      final aspectRatio = 1.5 / 1.0; // Width / Height
+
+                      // Calculate the dimensions of the floorplan
+                      final floorplanWidth = constraints.maxWidth * 1.0;
+                      final floorplanHeight = floorplanWidth / aspectRatio;
+
+                      // Calculate the size of the selectable regions
+                      final dotSize = 10.0; // Size of the selectable regions
+
+                      // Map the coordinates to the new coordinate system
+                      double mapCoordinate(double value, double minValue, double maxValue, double minPixel, double maxPixel) {
+                        return (value - minValue) / (maxValue - minValue) * (maxPixel - minPixel) + minPixel;
+                      }
+
+                      return InteractiveViewer(
+                        constrained: true,
+                        minScale: 0.5,
+                        maxScale: 1.0,
+                        child: GestureDetector(
+                          onTapDown: (details) {
+                            // Get the tap position relative to the widget
+                            final RenderBox box = context.findRenderObject() as RenderBox;
+                            final localOffset = box.globalToLocal(details.globalPosition);
+                            final tapX = localOffset.dx;
+                            final tapY = localOffset.dy;
+
+                            // Map the tap coordinates to the original coordinate system
+                            final mappedX = (tapX / floorplanWidth) * 1.7 - 0.1;
+                            final mappedY = (tapY / floorplanHeight) * 1.2;
+
+                            print('Tap position: $mappedX, $mappedY');
+
+                            // Determine which region was tapped
+                            for (myRegion.SelectableRegion region in myRegion.getSelectableRegions(floorplanWidth, floorplanHeight, mapCoordinate, dotSize, _onRegionTap)) {
+                              final regionLeft = region.left;
+                              final regionTop = region.top;
+                              final regionRight = regionLeft + region.width;
+                              final regionBottom = regionTop + region.height;
+
+                              if (tapX >= regionLeft && tapX <= regionRight &&
+                                  tapY >= regionTop && tapY <= regionBottom) {
+                                region.onTap();
+                                break;
+                              }
+                            }
+                          },
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Container(
+                                width: floorplanWidth,
+                                height: floorplanHeight,
+                                child: Image.asset(
+                                  'assets/map/floorplan.jpg',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              ...myRegion.getSelectableRegions(floorplanWidth, floorplanHeight, mapCoordinate, dotSize, _onRegionTap).map((region) {
+                                return Positioned(
+                                  left: region.left,
+                                  top: region.top,
+                                  child: GestureDetector(
+                                    onTap: region.onTap,
+                                    child: Container(
+                                      width: region.width,
+                                      height: region.height,
+                                      color: region.color,
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Selected Category: $selectedCategory', // Display the selected category
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/Map/region.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+class SelectableRegion {
+  final double left;
+  final double top;
+  final double width;
+  final double height;
+  final Color color;
+  final VoidCallback onTap;
+
+  SelectableRegion({
+    required this.left,
+    required this.top,
+    required this.width,
+    required this.height,
+    required this.color,
+    required this.onTap,
+  });
+}
+
+List<SelectableRegion> getSelectableRegions(
+    double floorplanWidth, double floorplanHeight, double Function(double, double, double, double, double) mapCoordinate, double dotSize, Function(String) onRegionTap) {
+  return [
+    SelectableRegion(
+      left: mapCoordinate(1.0, -0.2, 1.7, 0.0, floorplanWidth) - dotSize / 2,
+      top: mapCoordinate(1.0, -0.2, 1.2, 0.0, floorplanHeight) - dotSize / 2,
+      width: mapCoordinate(1.4, -0.2, 1.7, 0.0, floorplanWidth) - mapCoordinate(1.0, -0.2, 1.7, 0.0, floorplanWidth),
+      height: mapCoordinate(1.1, -0.2, 1.2, 0.0, floorplanHeight) - mapCoordinate(1.0, -0.2, 1.2, 0.0, floorplanHeight),
+      color: Colors.orange.withOpacity(0.7),
+      onTap: () {
+        print('Tapped on Pets Care region');
+        onRegionTap('Pets Care');
+      },
+    ),
+    SelectableRegion(
+      left: mapCoordinate(0.5, -0.2, 1.7, 0.0, floorplanWidth) - dotSize / 2,
+      top: mapCoordinate(1.0, -0.2, 1.2, 0.0, floorplanHeight) - dotSize / 2,
+      width: mapCoordinate(0.9, -0.2, 1.7, 0.0, floorplanWidth) - mapCoordinate(0.5, -0.2, 1.7, 0.0, floorplanWidth),
+      height: mapCoordinate(1.1, -0.2, 1.2, 0.0, floorplanHeight) - mapCoordinate(1.0, -0.2, 1.2, 0.0, floorplanHeight),
+      color: Colors.blue.withOpacity(0.7),
+      onTap: () {
+        print('Tapped on Hair Care region');
+        onRegionTap('Hair Care');
+      },
+    ),
+    SelectableRegion(
+      left: mapCoordinate(0.5, -0.2, 1.7, 0.0, floorplanWidth) - dotSize / 2,
+      top: mapCoordinate(0.0, -0.2, 1.2, 0.0, floorplanHeight) - dotSize / 2,
+      width: mapCoordinate(0.9, -0.2, 1.7, 0.0, floorplanWidth) - mapCoordinate(0.5, -0.2, 1.7, 0.0, floorplanWidth),
+      height: mapCoordinate(0.1, -0.2, 1.2, 0.0, floorplanHeight) - mapCoordinate(0.0, -0.2, 1.2, 0.0, floorplanHeight),
+      color: Colors.yellow.withOpacity(0.7),
+      onTap: () {
+        print('Tapped on Groceries region');
+        onRegionTap('Groceries');
+      },
+    ),
+    SelectableRegion(
+      left: mapCoordinate(1.0, -0.2, 1.7, 0.0, floorplanWidth) - dotSize / 2,
+      top: mapCoordinate(0.0, -0.2, 1.2, 0.0, floorplanHeight) - dotSize / 2,
+      width: mapCoordinate(1.4, -0.2, 1.7, 0.0, floorplanWidth) - mapCoordinate(1.0, -0.2, 1.7, 0.0, floorplanWidth),
+      height: mapCoordinate(0.1, -0.2, 1.2, 0.0, floorplanHeight) - mapCoordinate(0.0, -0.2, 1.2, 0.0, floorplanHeight),
+      color: Colors.green.withOpacity(0.7),
+      onTap: () {
+        print('Tapped on Make Up region');
+        onRegionTap('Make Up');
+      },
+    ),
+    SelectableRegion(
+      left: mapCoordinate(1.5, -0.2, 1.7, 0.0, floorplanWidth) - dotSize / 2,
+      top: mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight) - dotSize / 2,
+      width: mapCoordinate(1.6, -0.2, 1.7, 0.0, floorplanWidth) - mapCoordinate(1.5, -0.2, 1.7, 0.0, floorplanWidth),
+      height: mapCoordinate(0.7, -0.2, 1.2, 0.0, floorplanHeight) - mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight),
+      color: Color.fromARGB(255, 223, 5, 111).withOpacity(0.7),
+      onTap: () {
+        print('Tapped on Nutrition region');
+        onRegionTap('Nutrition');
+      },
+    ),
+    SelectableRegion(
+      left: mapCoordinate(1.15, -0.2, 1.7, 0.0, floorplanWidth) - dotSize / 2,
+      top: mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight) - dotSize / 2,
+      width: mapCoordinate(1.2, -0.2, 1.7, 0.0, floorplanWidth) - mapCoordinate(1.15, -0.2, 1.7, 0.0, floorplanWidth),
+      height: mapCoordinate(0.7, -0.2, 1.2, 0.0, floorplanHeight) - mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight),
+      color: Colors.grey.withOpacity(0.7),
+      onTap: () {
+        print('Tapped on Supplement region');
+        onRegionTap('Supplement');
+      },
+    ),
+    SelectableRegion(
+      left: mapCoordinate(1.1, -0.2, 1.7, 0.0, floorplanWidth) - dotSize / 2,
+      top: mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight) - dotSize / 2,
+      width: mapCoordinate(1.15, -0.2, 1.7, 0.0, floorplanWidth) - mapCoordinate(1.1, -0.2, 1.7, 0.0, floorplanWidth),
+      height: mapCoordinate(0.7, -0.2, 1.2, 0.0, floorplanHeight) - mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight),
+      color: Color.fromARGB(255, 16, 180, 202).withOpacity(0.7),
+      onTap: () {
+        print('Tapped on Tonic region');
+        onRegionTap('Tonic');
+      },
+    ),
+    SelectableRegion(
+      left: mapCoordinate(0.7, -0.2, 1.7, 0.0, floorplanWidth) - dotSize / 2,
+      top: mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight) - dotSize / 2,
+      width: mapCoordinate(0.75, -0.2, 1.7, 0.0, floorplanWidth) - mapCoordinate(0.7, -0.2, 1.7, 0.0, floorplanWidth),
+      height: mapCoordinate(0.7, -0.2, 1.2, 0.0, floorplanHeight) - mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight),
+      color: const Color.fromARGB(255, 176, 39, 55).withOpacity(0.7),
+      onTap: () {
+        print('Tapped on Foot Treatment region');
+        onRegionTap('Foot Treatment');
+      },
+    ),
+    SelectableRegion(
+      left: mapCoordinate(0.65, -0.2, 1.7, 0.0, floorplanWidth) - dotSize / 2,
+      top: mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight) - dotSize / 2,
+      width: mapCoordinate(0.7, -0.2, 1.7, 0.0, floorplanWidth) - mapCoordinate(0.65, -0.2, 1.7, 0.0, floorplanWidth),
+      height: mapCoordinate(0.7, -0.2, 1.2, 0.0, floorplanHeight) - mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight),
+      color: Color.fromARGB(255, 43, 205, 28).withOpacity(0.7),
+      onTap: () {
+        print('Tapped on Traditional Medicine region');
+        onRegionTap('Traditional Medicine');
+      },
+    ),
+    SelectableRegion(
+      left: mapCoordinate(0.25, -0.2, 1.7, 0.0, floorplanWidth) - dotSize / 2,
+      top: mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight) - dotSize / 2,
+      width: mapCoordinate(0.3, -0.2, 1.7, 0.0, floorplanWidth) - mapCoordinate(0.25, -0.2, 1.7, 0.0, floorplanWidth),
+      height: mapCoordinate(0.7, -0.2, 1.2, 0.0, floorplanHeight) - mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight),
+      color: const Color.fromARGB(255, 176, 128, 39).withOpacity(0.7),
+      onTap: () {
+        print('Tapped on Coffee region');
+        onRegionTap('Coffee');
+      },
+    ),
+    SelectableRegion(
+      left: mapCoordinate(0.2, -0.2, 1.7, 0.0, floorplanWidth) - dotSize / 2,
+      top: mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight) - dotSize / 2,
+      width: mapCoordinate(0.25, -0.2, 1.7, 0.0, floorplanWidth) - mapCoordinate(0.2, -0.2, 1.7, 0.0, floorplanWidth),
+      height: mapCoordinate(0.7, -0.2, 1.2, 0.0, floorplanHeight) - mapCoordinate(0.3, -0.2, 1.2, 0.0, floorplanHeight),
+      color: Color.fromARGB(255, 39, 41, 176).withOpacity(0.7),
+      onTap: () {
+        print('Tapped on Dairy Product region');
+        onRegionTap('Dairy Product');
+      },
+    ),
+  ];
+}
